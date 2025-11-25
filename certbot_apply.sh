ddb6b0090ccc6bf0debafc7a059ab952d23836c7
jiejia2009 <jiejia2009@gmail.com>
2025-11-25 12:30:43 +0800

    update certbot_apply.sh to use dynamic email variable and add nginx reload command

--- certbot_apply.sh
+++ certbot_apply.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 
-docker-compose up -d nginx
+docker-compose up 
 
-docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d aripplesong.me -d www.aripplesong.me --email jiejia2009@gmail.com --agree-tos --non-interactive
+rm -rf ./certbot/conf/live/*
 
-docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d podcast.aripplesong.me --email jiejia2009@gmail.com --agree-tos --non-interactive
+docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d aripplesong.me -d www.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
 
-docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d doc.podcast.aripplesong.me --email jiejia2009@gmail.com --agree-tos --non-interactive
+docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
 
-docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d cn.podcast.aripplesong.me --email jiejia2009@gmail.com --agree-tos --non-interactive
+docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d doc.podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
+
+docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d cn.podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
+
+docker-compose exec nginx nginx -s reload
 
 docker-compose down
--- init.sh
+++ init.sh
@@ -26,6 +26,10 @@ EOF
 
 # certbot apply script
 cat <<EOF > certbot_apply.sh
+docker-compose up
+
+rm -rf ./certbot/conf/live/*
+
 docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d aripplesong.me -d www.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
 
 docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
@@ -33,6 +37,11 @@ docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d podcas
 docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d doc.podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
 
 docker-compose run --rm certbot certonly --webroot -w /var/www/certbot -d cn.podcast.aripplesong.me --email ${certbot_email} --agree-tos --non-interactive
+
+docker-compose exec nginx nginx -s reload
+
+docker-compose down
+
 EOF
 
 chmod +x certbot_apply.sh
